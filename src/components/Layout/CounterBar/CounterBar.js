16fdbcd5bc3317527137676c31d3ee3f11a984d1
Oscar <bo.walczak@wp.pl>
2023-01-23 22:08:00 +0100

    counter

--- src/components/Layout/CounterBar/CounterBar.js
+++ src/components/Layout/CounterBar/CounterBar.js
@@ -1,20 +1,48 @@
 import React from "react";
 import classes from "./CounterBar.module.scss";
 
+const dateDiff = (date1, date2) => {
+  const diff = Math.abs(date2 - date1);
+
+  const monthInMs = 1000 * 60 * 60 * 24 * 30.4375;
+
+  const months = Math.floor(diff / monthInMs);
+
+  const weeks = Math.floor((diff % monthInMs) / (1000 * 60 * 60 * 24 * 7));
+
+  const days = Math.floor((diff % monthInMs) / (1000 * 60 * 60 * 24));
+
+  return { months, weeks, days };
+};
+
 const CounterBar = () => {
+  const startingDay = new Date("2022-11-06");
+  const currentDay = new Date();
+
+  const {
+    months: monthsOfLearning,
+    weeks: weeksOfLearning,
+    days: dayOfLearning,
+  } = dateDiff(startingDay, currentDay);
+
+  console.log(monthsOfLearning);
+
   return (
     <section className={classes.counter}>
-      <div>
-        <h3>Months of study</h3>
-        <p>3</p>
-      </div>
-      <div>
-        <h3>Weeks of study</h3>
-        <p>21</p>
-      </div>
-      <div>
-        <h3>Days of study</h3>
-        <p>147</p>
+      <h3 className={classes.counter__title}>{`< Time of daily learning />`}</h3>
+      <div className={classes.counter__digits}>
+        <div>
+          <p>{monthsOfLearning}</p>
+          <h3>Months</h3>
+        </div>
+        <div>
+          <p>{weeksOfLearning}</p>
+          <h3>Weeks</h3>
+        </div>
+        <div>
+          <p>{dayOfLearning}</p>
+          <h3>Days</h3>
+        </div>
       </div>
     </section>
   );
