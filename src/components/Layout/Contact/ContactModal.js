cba53cb7921caeb215aebe23b0fde5d44582ba91
Oscar <bo.walczak@wp.pl>
2023-02-07 22:41:15 +0100

    move nav, modal, Contact modal

---
+++ src/components/Layout/Contact/ContactModal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from "./ContactModal.module.scss";
+
+
+const ContactModal = () => {
+  return (
+    <div>ContactModal</div>
+  )
+}
+
+export default ContactModal
---
+++ src/components/Layout/Header/Nav/Nav.js
@@ -0,0 +1,21 @@
+import classes from "./Nav.module.scss";
+
+const Nav = ({ isMenu, menuToggle }) => {
+  return (
+    <nav className={isMenu ? classes.menu__nav : classes.nav}>
+      <ul>
+      <li onClick={menuToggle}>
+        <a href="#about-me">About</a>
+      </li>
+        <li onClick={menuToggle}>
+          <a href="https://github.com/oscarwallshack">Github</a>
+        </li>
+        <li onClick={menuToggle}>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
