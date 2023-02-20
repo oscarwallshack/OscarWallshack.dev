98f2550f373e1bcafa23f7f98df3d89e3fc15567
Oscar <bo.walczak@wp.pl>
2023-02-20 19:12:32 +0100

    gtm-module

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -2,6 +2,14 @@ import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Layout/Footer/Footer";
 import Header from "../components/Layout/Header/Header";
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-W6G2VVJ'
+}
+
+TagManager.initialize(tagManagerArgs)
+
 
 const Root = () => {
   return (
