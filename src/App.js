42562f0005fe492996b7cf60784062ab949b22f0
Oscar <bo.walczak@wp.pl>
2023-02-20 21:54:29 +0100

    lazyloading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AboutMePage from "./pages/AboutMe";
+// import AboutMePage from "./pages/AboutMe";
 import HomePage from "./pages/Home";
 import Root from "./pages/Root";
+
+const AboutMePage = lazy(() => import("./pages/AboutMe"));
 
 const router = createBrowserRouter([
   {
@@ -9,7 +12,14 @@ const router = createBrowserRouter([
     element: <Root />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: 'about-me', element: <AboutMePage /> },
+      {
+        path: "about-me",
+        element: (
+          <Suspense fallback={<p>Loading..</p>}>
+            <AboutMePage />
+          </Suspense>
+        ),
+      },
     ],
   },
 ]);
