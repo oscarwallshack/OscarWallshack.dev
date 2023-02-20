c3104459f7297cf79612cdb04ecc095f79220b7d
Oscar <bo.walczak@wp.pl>
2023-02-20 21:43:41 +0100

    glitch photo

--- src/components/Layout/Introduce/Teaser.js
+++ src/components/Layout/Introduce/Teaser.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./Teaser.module.scss";
-import profile from "../../../assets/prof_glitch.webp";
+import glitchProfile from "../../../assets/prof_glitch.webp";
+import profile from "../../../assets/prof.webp";
 import Button from "../../UI/Button/Button";
 import { Link } from "react-router-dom";
 
 const Teaser = () => {
+  const [profileImg, setProfileImg] = useState(glitchProfile);
+
+  const handleMouseOver = () => {
+    setProfileImg(profile);
+  };
+
+  const handleMouseOut = () => {
+    setProfileImg(glitchProfile);
+  };
+
   return (
     <section className={classes.teaser}>
-        <div>
-          <h2>More about me</h2>
-          <Link to={"/about-me"}>
-            <Button text="About me" />
-          </Link>
-        </div>
-        <img src={profile} alt="profile" className={classes.prof} />
+      <div>
+        <h2>More about me</h2>
+        <Link to={"/about-me"}>
+          <Button text="About me" />
+        </Link>
+      </div>
+      <img
+        src={profileImg}
+        alt="profile"
+        className={classes.prof}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      />
     </section>
   );
 };
